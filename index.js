d6b169c68cb0226742e967c34ee391db3082b491
basant10m <basant@onepercentstartups.com>
2023-12-12 19:27:17 +0530

    Object and people

--- index.js
+++ index.js
@@ -15,3 +15,42 @@ function changeTab(tab) {
 }
 
 changeTab(currentTab);
+
+function changeName() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("name");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("peoplestable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+          } else {
+              tr[i].style.display = "none";
+          }
+      }
+  }
+}
+
+
+function objectName() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("objectname");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("peoplestable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+          } else {
+              tr[i].style.display = "none";
+          }
+      }
+  }
+}
